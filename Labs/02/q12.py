85a3047ed1dca88490ec4171b3c02d2a1f12646a
Muhammad Umar <134300686+M-Umar4002@users.noreply.github.com>
2024-08-29 01:08:34 +0500

    Create q12.py

---
+++ Labs/02/q12.py
@@ -0,0 +1,21 @@
+def take_input(size):
+    thisList = []
+    for i in range(size):
+        thisList.append(input(f"Enter element {i + 1} : "))
+    return thisList
+
+def make_dict(list1, list2):
+    dic = {}
+    for i in range(len(list1)):
+        dic[list1[i]] = list2[i]
+    return dic
+
+size = int(input("Enter total number of elements : "))
+
+print("\nEnter elements of list1")
+list1 = take_input(size)
+
+print("\nEnter elements of list2")
+list2 = take_input(size)
+
+print("\nDictionary :", make_dict(list1, list2))
