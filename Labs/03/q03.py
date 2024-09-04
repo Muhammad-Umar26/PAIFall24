31abfcfe67e4446a02e6cfdd3d71d8e60a675ea3
Muhammad Umar <134300686+Muhammad-Umar26@users.noreply.github.com>
2024-09-04 09:45:48 +0500

    Create q03.py

---
+++ Labs/03/q03.py
@@ -0,0 +1,25 @@
+def take_input(size):
+    thisList = []
+    for i in range(size):
+        thisList.append(input(f"Enter element {i + 1} : "))
+    return thisList
+
+def make_dict(list1, list2):
+    dic = {}
+    for i in range(len(list1)):
+        dic[list1[i]] = list2[i]
+    return dic
+3
+size = int(input("Enter total number of elements : "))
+
+print("\nEnter elements of list1")
+list1 = take_input(size)
+
+print("\nEnter elements of list2")
+list2 = take_input(size)
+
+f = open("file.txt", 'w')
+
+f.write(str(make_dict(list1, list2)))
+
+f.close()
