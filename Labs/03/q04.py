9eee5639fc54ca6670aa44d8a8175d0957a3c762
Muhammad Umar <134300686+Muhammad-Umar26@users.noreply.github.com>
2024-09-06 02:39:13 +0500

    Update q04.py

--- Labs/03/q04.py
+++ Labs/03/q04.py
@@ -1,28 +1,22 @@
-name = input("Enter biodata of employee\nName : ")
-cnic = input("CNIC : ")
-age = int(input("Age : "))
-salary = int(input("Salary : "))
-
-f = open("file.txt", 'w')
-
-f.write(f"Name: {name}\nCNIC: {cnic}\nAge: {age}\nSalary: {salary}")
-
-f.close()
-
-conNum = input("Contact Number : ")
-
-f = open("file.txt", 'a')
-
-f.write(f"\nContact Number: {conNum}")
-
-f.close()
-
 try:
-    f = open("file.txt")
-    
-    print("\nReading from file\n" + f.read())
+    name = input("Enter biodata of employee\nName : ")
+    cnic = input("CNIC : ")
+    age = int(input("Age : "))
+    salary = int(input("Salary : "))
 
+    f = open("q04.txt", 'w')
+    f.write(f"Name: {name}\nCNIC: {cnic}\nAge: {age}\nSalary: {salary}")
     f.close()
 
-except FileNotFoundError:
-    print("Error: The file 'file.txt' was not found.")
+    conNum = input("Contact Number : ")
+
+    f = open("q04.txt", 'a')
+    f.write(f"\nContact Number: {conNum}")
+    f.close()
+
+    f = open("q04.txt")
+    print("\nReading from file\n" + f.read())
+    f.close()
+
+except Exception as e:
+    print(str(e))
