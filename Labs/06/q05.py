73ac382b0a1cdfb870a57cca1f28ef31f5994a82
Muhammad Umar <134300686+Muhammad-Umar26@users.noreply.github.com>
2024-09-29 06:05:59 +0500

    Update q05.py

--- Labs/06/q05.py
+++ Labs/06/q05.py
@@ -1,14 +1,5 @@
-import pandas as pd   
+import pandas as pd
 
-data = {
-    'year': [1988, 1986, 1985],
-    'WHO region': ['Western Pecific', 'Americans', 'Africa'],
-    'Country': ['Veit Nam', 'Uruguay', "Cle d'Ivoire"],
-    'Beverage types': ['Wine', 'Other', 'Wine'],
-    'Display Value': [0, 0.5, 1.62]
-}
-
-df = pd.DataFrame(data)
+df = pd.read_csv('world_alcohol.csv')
 print(df.shape)
-
 print(df.columns)
