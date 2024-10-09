0a520f086c7b3f7be97377acb8ae0cdbab5a7c1f
Muhammad Umar <134300686+Muhammad-Umar26@users.noreply.github.com>
2024-10-10 01:59:30 +0500

    Update q05.py

--- Labs/08/q05.py
+++ Labs/08/q05.py
@@ -1,16 +1,5 @@
 import numpy as np
 
-arr = np.random.choice([2,5,9,10],size = (4,4))
+arr = (np.random.choice([2, 5, 9, 10], size=(4, 4)) * np.eye(4)) + np.arange(1, 33, 2).reshape(4, 4)
 
-a = np.multiply(arr,np.eye(4,4))
-
-arr2 = np.arange(1,32,2)
-
-b = arr2.reshape(4,4)
-
-c = np.add(a,b)
-
-print(arr,'\n')
-print(a,'\n')
-print(b,'\n')
-print(c,'\n')
+print(arr)
