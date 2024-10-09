c48de6f38ae4baae35ffdd8a6585dcdf47002780
Muhammad Umar <134300686+Muhammad-Umar26@users.noreply.github.com>
2024-10-10 01:38:19 +0500

    Update q03.py

--- Labs/08/q03.py
+++ Labs/08/q03.py
@@ -1,18 +1,5 @@
 import numpy as np
 
-arr = np.arange(2,19,2)
+arr = (np.arange(2, 20, 2).reshape(3, 3) * 4) * np.eye(3)
 
-a = arr.reshape(3,3)
-
-def mul(x,y):
-    return x*y
-
-myMul = np.frompyfunc(mul,2,1)
-
-b = myMul(a,4)
-
-c = np.multiply(np.eye(3,3),b)
-
-print(a,'\n')
-print(b,'\n')
-print(c)
+print(arr)
