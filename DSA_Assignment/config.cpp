62bf4482b1281f79690f412823660b575627e45c
Muhammad Umar <134300686+Muhammad-Umar26@users.noreply.github.com>
2024-11-14 04:56:42 +0500

    Create config.cpp

---
+++ DSA_Assignment/config.cpp
@@ -0,0 +1,4 @@
+// Initialize configuration variables
+int elementarySortingAlgorithm = 0; // Default to Bubble Sort
+int advancedSortingAlgorithm = 3;   // Default to Merge Sort
+int threshold = 50;                 // threshold value less than 100 will enable the selected elementary sorting and greater value than 100 will enable selected advanced sorting algo
